1daa01d453690e77772557ad7f98c601c0dc2fed
Ben Sedat <bsedat@blackduck.com>
2024-11-13 16:01:23 -0800

    Upgrade to Rails 8.0.0

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_09_06_225059) do
+ActiveRecord::Schema[8.0].define(version: 2024_11_13_235215) do
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -25,5 +25,4 @@ ActiveRecord::Schema[7.1].define(version: 2024_09_06_225059) do
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
-
 end
